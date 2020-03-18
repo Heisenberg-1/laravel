02259290b352600ca1631cbf507d1a726c981c46
Erfan-Naseri <er9.naseri@gmail.com>
2020-03-18 14:36:12 +0330

    add attribute to user.php

--- app/User.php
+++ app/User.php
@@ -19,6 +19,8 @@ class User extends Authenticatable
         'name', 'email', 'password',
     ];
 
+   protected $attributes=['name'=>'erfan'];
+
     /**
      * The attributes that should be hidden for arrays.
      *
